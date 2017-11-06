0605b1a31a78f7669d4d5515f2495132f3496e61
Edison Lee <edisonlee@edisonlee55.com>
2017-11-06 09:24:12 +0800

    Update verifyToken

--- bottender.config.js
+++ bottender.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   messenger: {
     accessToken: 'EAAEJ3FEWgZAkBAA8soHRpZAlLZACY3wYZAwFI0Pdtp1AXdHphsVAicXLeEzGXvF2kcfbKH8U5nGCp8Hi4pYVvGBMS3BiOrrmHJGKp3azy85PKwlAwXPOS8SDiSERTLT94XRpknNnKsiYGiHmfFbjtTIbcD1JTe3xvOfXPOGyPtZCCkDNm9i9Q',
-    verifyToken: '__PUT_YOUR_VERITY_TOKEN_HERE__',
+    verifyToken: 'cfwae8wAnFyV6jO486hLlGYU6C8jXO4',
     appId: '292316834595225',
     appSecret: '5ac7ab4daffb4ce78c293d1092ef7e13'
   }
