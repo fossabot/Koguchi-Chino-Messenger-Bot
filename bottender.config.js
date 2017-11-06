2403157043185e8ed4c1d568df169036d02c4f30
Edison Lee <edisonlee@edisonlee55.com>
2017-11-06 09:23:13 +0800

    Init bottender

---
+++ bottender.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  messenger: {
+    accessToken: 'EAAEJ3FEWgZAkBAA8soHRpZAlLZACY3wYZAwFI0Pdtp1AXdHphsVAicXLeEzGXvF2kcfbKH8U5nGCp8Hi4pYVvGBMS3BiOrrmHJGKp3azy85PKwlAwXPOS8SDiSERTLT94XRpknNnKsiYGiHmfFbjtTIbcD1JTe3xvOfXPOGyPtZCCkDNm9i9Q',
+    verifyToken: '__PUT_YOUR_VERITY_TOKEN_HERE__',
+    appId: '292316834595225',
+    appSecret: '5ac7ab4daffb4ce78c293d1092ef7e13'
+  }
+};
---
+++ index.js
@@ -0,0 +1,20 @@
+const { MessengerBot, FileSessionStore } = require('bottender');
+const { createServer } = require('bottender/express');
+
+const config = require('./bottender.config.js').messenger;
+
+const bot = new MessengerBot({
+  accessToken: config.accessToken,
+  appSecret: config.appSecret,
+  sessionStore: new FileSessionStore(),
+});
+
+bot.onEvent(async context => {
+  await context.sendText('Hello World');
+});
+
+const server = createServer(bot);
+
+server.listen(5000, () => {
+  console.log('server is running on 5000 port...');
+});
