ab0bf071de6b9408c18d53c6c1e0be7aee7a4bec
Edison Lee <edisonlee@edisonlee55.com>
2017-11-07 09:44:51 +0800

    Add Quick Replies

--- bot.js
+++ bot.js
@@ -130,13 +130,16 @@ function receivedPostback(event) {
   // The 'payload' param is a developer-defined field which is set in a postback 
   // button for Structured Messages. 
   var payload = event.postback.payload;
-
   console.log("Received postback for user %d and page %d with payload '%s' " +
     "at %d", senderID, recipientID, payload, timeOfPostback);
 
-  // When a postback is called, we'll send a message back to the sender to 
-  // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
+  switch (payload) {
+    case 'SEND_LOLI_PHOTO':
+      sendLoliPhoto(recipientID);
+      break;
+    default:
+      console.error("Unexpected payload: " + payload);
+  }
 }
 
 //////////////////////////
@@ -148,7 +151,14 @@ function sendTextMessage(recipientId, messageText) {
       id: recipientId
     },
     message: {
-      text: messageText
+      text: "請選擇您需要的功能:",
+      quick_replies: [
+        {
+          content_type: "text",
+          title: "來張蘿莉照片!",
+          payload: "SEND_LOLI_PHOTO",
+        }
+      ]
     }
   };
 
